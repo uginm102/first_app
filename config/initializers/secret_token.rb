0579662bf7393c3ff12f5885b46c65d89b08bb13
Eugene Munyaneza <munyanezae@gmail.com>
2013-09-14 16:09:19 +0300

    Securandom to SecureRandom

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -18,7 +18,7 @@ def secure_token
     File.read(token_file).chomp
   else
     # Generate a new token and store it in token_file
-    token = Securerandom.hex(64)
+    token = SecureRandom.hex(64)
     File.write(token_file, token)
     token
   end
