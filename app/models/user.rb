27929496dcf7b38dc2b967021916fcd8b41ddf39
Eugene Munyaneza <munyanezae@gmail.com>
2013-09-22 17:56:34 +0300

    fixed a few tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6}
   
-  before_save { self.email = email.downcase}
+  before_save { self.email.downcase!}
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -67,17 +67,14 @@ describe User do
       it { should_not be_valid }
     end
     
-    #describe "saves email as lower case" do
-      #let(:user) { @user }
-      #before do
-        #:uppercase_email = @user.email.upcase
-       #@user.email = :uppercase_email
-       #@user.save
-       #:saved_email = @user.email
-     #end
-     #:uppercase_email.should_not eq :email
-     #it { user.email eq user.email.downcase }
-   #end
+    describe "email address with mixed case" do
+      let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+      it "should be saved as all lower-case" do
+        @user.email = mixed_case_email
+        @user.save
+       expect(@user.reload.email).to eq mixed_case_email.downcase
+     end
+   end
 
    describe "return value of authenticate method" do
      before { @user.save }
